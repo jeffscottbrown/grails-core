9b1b698e748ca7674fc0eda5e71499343295c000
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-11-17 20:41:06 -0800

    auto configure messageSource

--- grails-plugin-i18n/src/main/groovy/org/grails/plugins/i18n/I18nAutoConfiguration.java
+++ grails-plugin-i18n/src/main/groovy/org/grails/plugins/i18n/I18nAutoConfiguration.java
@@ -1,19 +1,40 @@
 package org.grails.plugins.i18n;
 
+import grails.config.Settings;
+import grails.core.GrailsApplication;
+import grails.plugins.GrailsPluginManager;
+import grails.util.Environment;
+import org.grails.spring.context.support.PluginAwareResourceBundleMessageSource;
 import org.grails.web.i18n.ParamsAwareLocaleChangeInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-@AutoConfiguration(before = { WebMvcAutoConfiguration.class })
+@AutoConfiguration(before = { MessageSourceAutoConfiguration.class, WebMvcAutoConfiguration.class })
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class I18nAutoConfiguration {
+
+    @Value("${" + Settings.GSP_VIEW_ENCODING + ":UTF-8}")
+    private String encoding;
+
+    @Value("${" + Settings.GSP_ENABLE_RELOAD + ":false}")
+    private boolean gspEnableReload;
+
+    @Value("${" + Settings.I18N_CACHE_SECONDS + ":5}")
+    private int cacheSeconds;
+
+    @Value("${" + Settings.I18N_FILE_CACHE_SECONDS + ":5}")
+    private int fileCacheSeconds;
 
     @Bean
     @ConditionalOnMissingBean(name = DispatcherServlet.LOCALE_RESOLVER_BEAN_NAME)
@@ -26,5 +47,17 @@ public class I18nAutoConfiguration {
         ParamsAwareLocaleChangeInterceptor localeChangeInterceptor = new ParamsAwareLocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         return localeChangeInterceptor;
+    }
+
+    @Bean(AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME)
+    public MessageSource messageSource(GrailsApplication grailsApplication, GrailsPluginManager pluginManager) {
+        PluginAwareResourceBundleMessageSource messageSource = new PluginAwareResourceBundleMessageSource(grailsApplication, pluginManager);
+        messageSource.setDefaultEncoding(encoding);
+        messageSource.setFallbackToSystemLocale(false);
+        if (Environment.getCurrent().isReloadEnabled() || gspEnableReload) {
+            messageSource.setCacheSeconds(cacheSeconds);
+            messageSource.setFileCacheSeconds(fileCacheSeconds);
+        }
+        return messageSource;
     }
 }
